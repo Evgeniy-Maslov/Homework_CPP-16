4c28968660f77492b02c6696c73925deaa8ec375
Evgeniy Maslov <Evgeniy-Almas@yandex.ru>
2023-12-24 00:05:16 +0700

    Lesson_3

---
+++ Lesson_3/Задача 1. Калькулятор двух чисел/Задача 1. Калькулятор двух чисел/Задача 1. Калькулятор двух чисел.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+#include <windows.h>
+
+class Calculator
+{
+public:
+    double num1;
+    double num2;
+
+    double add()
+    {
+        return num1 + num2;
+    }
+    double multiply()
+    {
+        return num1 * num2;
+    }
+    double subtract_1_2()
+    {
+        return num1 - num2;
+    }
+    double subtract_2_1()
+    {
+        return num2 - num1;
+    }
+    double divide_1_2()
+    {
+        return num1 / num2;
+    }
+    double divide_2_1()
+    {
+        return num2 / num1;
+    }
+    bool set_num1(double num1)
+    {
+        if (num1 == 0)
+        {
+            std::cout << "Неверный ввод!";
+            std::cout << std::endl;
+            return false;
+        }
+        else
+        {
+            this->num1 = num1;
+        }
+        return true;
+    }
+    bool set_num2(double num2)
+    {
+        if (num2 == 0)
+        {
+            std::cout << "Неверный ввод!";
+            std::cout << std::endl;
+            return false;
+        }
+        else
+        {
+            this->num2 = num2;
+        }
+        return true;
+    }
+};
+
+int main(int argc, char** argv)
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    double n;
+    double m;
+    Calculator two_numbers;
+    do
+    {
+        std::cout << "Введите num1: ";
+        std::cin >> n;
+    } while (two_numbers.set_num1(n) == false);
+    do
+    {
+        std::cout << "Введите num2: ";
+        std::cin >> m;
+    } while (two_numbers.set_num2(m) == false);
+
+    std::cout << "num1 + num2 = " << two_numbers.add() << "\n";
+    std::cout << "num1 - num2 = " << two_numbers.subtract_1_2() << "\n";
+    std::cout << "num2 - num1 = " << two_numbers.subtract_2_1() << "\n";
+    std::cout << "num1 * num2 = " << two_numbers.multiply() << "\n";
+    std::cout << "num1 / num2 = " << two_numbers.divide_1_2() << "\n";
+    std::cout << "num2 / num1 = " << two_numbers.divide_2_1() << "\n";
+    return 0;
+}
---
+++ Lesson_3/Задача 2. Счётчик/Задача 2. Счётчик/Задача 2. Счётчик.cpp
@@ -0,0 +1,91 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+
+class Counter
+{
+public:
+	int count;
+	Counter()
+	{
+		count = 1;
+	}
+	Counter(int count)
+	{
+		this->count = count;
+	}
+	void action_Counter()
+	{
+		std::string symbol;
+		do
+		{
+			std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+			std::cin >> symbol;
+			calculation_Counter(count, symbol);				
+		} while (symbol != "x");
+		std::cout << "До свидания!";
+		std::cout << std::endl;
+	}
+private:
+	void calculation_Counter(int &count, std::string symbol)
+	{
+		if (symbol == "x")
+			;
+		else if (symbol == "+")
+			count++;
+		else if (symbol == "-")
+			count--;
+		else if (symbol == "=")
+			std::cout << count << std::endl;
+		else
+			std::cout << "Введите корректный символ" << std::endl;
+	}
+};
+
+void check_number(int &x)
+{
+	bool b = true;
+	do
+	{
+		std::string buff;
+		std::cout << "Введите начальное значение счётчика: ";
+		std::cin >> buff;
+		if ((buff.find_first_not_of("0123456789") == std::string::npos) == true)
+		{
+			x = std::stoi(buff);
+			b = false;
+		}
+		else
+		{
+			std::cout << "Вы ввели не число!\n";
+		}
+	} while (b);
+}
+
+int main(int argc, char** argv)
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	std::string yes_or_no;
+	int n{};
+	bool a = true;
+	do
+	{
+		std::cout << "Вы хотите указать начальное значение счётчика? Введите 'yes' или 'no': ";
+		std::cin >> yes_or_no;
+		if (yes_or_no == "yes")
+		{
+			a = false;
+			check_number(n);
+			Counter C(n);
+			C.action_Counter();
+		}
+		else if (yes_or_no == "no")
+		{
+			a = false;
+			Counter C;
+			C.action_Counter();
+		}
+	} while (a);
+	return 0;
+}
