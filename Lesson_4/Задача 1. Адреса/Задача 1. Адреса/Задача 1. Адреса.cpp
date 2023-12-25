de1a7af3b08cb5b91e570453d0d979e056385bf2
Evgeniy Maslov <Evgeniy-Almas@yandex.ru>
2023-12-25 14:38:53 +0700

    Lesson_4

---
+++ Lesson_4/Задача 1. Адреса/Задача 1. Адреса/Задача 1. Адреса.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <fstream>
+
+struct arr_address
+{
+    std::string City;
+    std::string street;
+    int house;
+    int apartment;
+};
+
+class Addresses
+{
+    int n;
+
+public:
+    void overwrite_file()
+    {
+        read_Addresses();
+    }
+private:
+    void read_Addresses()
+    {
+        std::ifstream fin("in.txt");
+        fin >> n;
+        arr_address* list = new arr_address[n];
+        for (int i = 0; i < n; i++)
+        {
+            fin >> list[i].City
+                >> list[i].street
+                >> list[i].house
+                >> list[i].apartment;
+        }
+        fin.close();
+        write_Address(n, list);
+    }
+    void write_Address(int n, arr_address* list)
+    {
+        std::ofstream fout("out.txt");
+        fout << n << std::endl;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (i == 0)
+            {
+                fout << list[i].City << ", "
+                    << list[i].street << ", "
+                    << list[i].house << ", "
+                    << list[i].apartment;
+            }
+            else
+            {
+                fout << list[i].City << ", "
+                    << list[i].street << ", "
+                    << list[i].house << ", "
+                    << list[i].apartment << std::endl;
+            }
+        }
+        fout.close();
+        delete[] list;
+    }
+};
+
+int main(int argc, char** argv)
+{
+    Addresses a;
+    a.overwrite_file();
+    return 0;
+}
---
+++ Lesson_4/Задача 2. Сортируем адреса/Задача 2. Сортируем адреса/Задача 2. Сортируем адреса.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <fstream>
+
+struct arr_address
+{
+    std::string City;
+    std::string street;
+    int house;
+    int apartment;
+};
+
+class Addresses
+{
+    int n;
+
+public:
+    void overwrite_file()
+    {
+        read_Addresses();
+    }
+private:
+    void read_Addresses()
+    {
+        std::ifstream fin("in.txt");
+        fin >> n;
+        arr_address* list = new arr_address[n];
+        for (int i = 0; i < n; i++)
+        {
+            fin >> list[i].City
+                >> list[i].street
+                >> list[i].house
+                >> list[i].apartment;
+        }
+        fin.close();
+        sorting_city(n, list);
+        write_Address(n, list);
+    }
+    void sorting_city(int n, arr_address* list)
+    {
+        for (int j = 0; j < n; j++) 
+        {
+            for (int i = n - 1; i > 0; i--) 
+            {
+                if (list[i].City < list[i - 1].City) 
+                {
+                    arr_address tmp = list[i];
+                    list[i] = list[i - 1];
+                    list[i - 1] = tmp;
+                }
+            }
+        }
+    }
+    void write_Address(int n, arr_address* list)
+    {
+        std::ofstream fout("out.txt");
+        fout << n << std::endl;
+        for (int i = 0; i < n; i++)
+        {
+            if (i == n - 1)
+            {
+            fout << list[i].City << ", "
+                << list[i].street << ", "
+                << list[i].house << ", "
+                << list[i].apartment;
+            }
+            else
+            {
+                fout << list[i].City << ", "
+                    << list[i].street << ", "
+                    << list[i].house << ", "
+                    << list[i].apartment << std::endl;
+            }
+        }
+        fout.close();
+        delete[] list;
+    }
+};
+
+int main(int argc, char** argv)
+{
+    Addresses a;
+    a.overwrite_file();
+    return 0;
+}
