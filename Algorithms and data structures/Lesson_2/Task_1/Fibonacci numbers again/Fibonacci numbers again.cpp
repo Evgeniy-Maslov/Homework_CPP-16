c8c4961583a73951ca00d2dc2fa191dc864aa7f3
Evgeniy Maslov <Evgeniy-Almas@yandex.ru>
2024-02-22 19:32:52 +0700

    Algorithms_Lesson_2

---
+++ Algorithms and data structures/Lesson_2/Task_1/Fibonacci numbers again/Fibonacci numbers again.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <format>
+
+
+int Fibonacci(int n)
+{
+    if (n <= 1) { return n; }
+    else
+    {
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+}
+
+int main(int argc, char** argv)
+{
+    int n;
+    std::cout << "Enter the Fibonacci sequence number: ";
+    std::cin >> n;
+    std::cout << std::format("Fibonacci number = {} \n", Fibonacci(n));
+    return 0;
+}
+
+//Каждый раз, когда мы вызываем Fibonacci(n), потенциально будет выполняться еще два вызова Fibonacci. 
+// И т.д. до базовых значений n.
+//Таким образом сложность работы = О(2^n). сложность использования памяти = О(1), т.к. количество памяти не зависит 
+// от количества входных данных.
---
+++ Algorithms and data structures/Lesson_2/Task_2/Accelerating Fibonacci/Accelerating Fibonacci.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <format>
+
+
+int Fibonacci(int n, int *memory)
+{
+    if (n <= 1) { return n; }
+    else if (memory[n -1] == 0)
+    {
+        memory[n - 1] = Fibonacci(n - 1, memory) + Fibonacci(n - 2, memory);
+        return memory[n - 1];
+    }
+    else { return memory[n - 1]; }
+}
+
+int main(int argc, char** argv)
+{
+    int n;
+    std::cout << "Enter the Fibonacci sequence number: ";
+    std::cin >> n;
+    int* memory = new int[n]();
+    memory[0] = 0;
+    memory[1] = 1;
+    std::cout << std::format("Fibonacci number = {} \n", Fibonacci(n, memory));
+    delete[] memory;
+    return 0;
+}
