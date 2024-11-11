ca93f86808c59f37fea050b94ef9d036c3c032cc
Evgeniy <Evgeniy-Almas@yandex.ru>
2024-11-11 22:00:38 +0700

    Advanced_Lesson_9

--- Advanced_Programming_Cpp/Lesson_9/Task_3/std_unique_ptr-with_your_hands/std_unique_ptr-with_your_hands.cpp
+++ Advanced_Programming_Cpp/Lesson_9/Task_3/std_unique_ptr-with_your_hands/std_unique_ptr-with_your_hands.cpp
@@ -5,10 +5,9 @@
 
 int main()
 {
-    
     std_unique_ptr<int> ptr(new int(5));
     std_unique_ptr<double> ptr_2(new double(10.25));
- ;
+ 
     std::cout << *ptr << "\n";
     std::cout << *ptr_2 << "\n";
     
@@ -20,17 +19,4 @@ int main()
     //std::cout << *ptr_2 << "\n";
 
     return 0;
-   /* std::cout << ptr->xy << "\n";
-    ptr2->print();
-    std::cout << summa(*ptr) << "\n";
-    std::cout << ptr2->xy << "\n";*/
-    //std::cout << 
-
-    /*std::vector<int>* A = new std::vector<int>(3, 5);
-    std::vector<int>::iterator it = A->begin();
-    while (it != A->end()) {
-        std::cout << *it << " ";
-        it++;
-    }
-    std::cout << "Hello World!\n";*/
 }
