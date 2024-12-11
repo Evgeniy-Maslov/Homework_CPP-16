7390eb5b13b2a47f21982bcbb1753980ddfdfd24
Evgeniy <Evgeniy-Almas@yandex.ru>
2024-12-11 16:20:34 +0700

    working_with_databases_Task_2

---
+++ working with databases/Lesson_2/Task_2/Employe.sql
@@ -0,0 +1,6 @@
+create table if not exists Employe(
+	Employe_Id serial primary key,
+	name varchar(20) not null,
+	department varchar(50) not null,
+	Boss_Id integer references Employe(Employe_Id)
+);
