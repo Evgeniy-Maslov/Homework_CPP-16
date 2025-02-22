cdb3efa0503cdd66f39ad81187592b9ad1d2ccf1
Evgeniy Maslov <Evgeniy-Almas@yandex.ru>
2025-02-23 00:57:41 +0700

    2_Structural_patterns

---
+++ Design_patterns_in_Cpp/2_Structural_patterns/Task_1/main.cpp
@@ -0,0 +1,105 @@
+﻿#include <string>
+#include <iostream>
+
+
+class SimpleText
+{
+public:
+	virtual void render(const std::string& data) const
+	{
+		std::cout << data;
+	}
+};
+
+
+class DecoratedText : public SimpleText
+{
+public:
+	SimpleText* _text_;
+	DecoratedText(SimpleText* text) : _text_(text) {}
+};
+
+class ItalicText : public DecoratedText	
+{
+public:
+	ItalicText(SimpleText* text) : DecoratedText(text) {}
+	void render(const std::string& data)	
+	{										
+		std::cout << "<i>";					
+		_text_->render(data);			
+		std::cout << "</i>";
+	}
+};
+
+class BoldText : public DecoratedText	
+{
+public:
+	BoldText(SimpleText* text) : DecoratedText(text) {}
+	void render(const std::string& data)	
+	{										
+		std::cout << "<b>";					
+		_text_->render(data);			
+		std::cout << "</b>";
+	}
+};
+
+class Paragraph : public DecoratedText
+{
+public:
+	Paragraph(SimpleText* text) : DecoratedText(text) {}
+	void render(const std::string& data)	
+	{										
+		std::cout << "<p>";					
+		_text_->render(data);			
+		std::cout << "</p>";
+	}
+};
+
+class Reversed : public DecoratedText
+{
+public:
+	Reversed(SimpleText* text) : DecoratedText(text) {}
+	void render(const std::string& data)
+	{
+		std::string revers_data = data;
+		std::reverse(revers_data.begin(), revers_data.end());
+		_text_->render(revers_data);
+	}
+};
+
+class Link : public DecoratedText
+{
+public:
+	Link(SimpleText* text) : DecoratedText(text) {}
+	void render(const std::string& data_1, const std::string& data_2)
+	{
+		std::cout << "<a href = " + data_1 + ">";
+		_text_->render(data_2);
+		std::cout << "</a>";
+	}
+};
+
+	
+int main()
+{
+	auto simple_text = new SimpleText();
+	simple_text->render("HTML code");
+	std::cout << std::endl;
+	auto italic_text = new ItalicText(simple_text);
+	italic_text->render("HTML code italic style");
+	std::cout << std::endl;
+	auto bold_text = new BoldText(simple_text);
+	bold_text->render("HTML code bold style");
+	std::cout << std::endl;
+
+	auto paragraph_text = new Paragraph(simple_text);
+	paragraph_text->render("Hello world");
+	std::cout << std::endl;
+	auto reversed_text = new Reversed(simple_text);
+	reversed_text->render("Hello world");
+	std::cout << std::endl;
+	auto link_text = new Link(simple_text);
+	link_text->render("netology.ru", "Hello world"); 
+	std::cout << std::endl;
+	return 0;
+}
---
+++ Design_patterns_in_Cpp/2_Structural_patterns/Task_2/main.cpp
@@ -0,0 +1,81 @@
+﻿#include <map>
+#include <string>
+#include <iostream>
+
+class VeryHeavyDatabase
+{
+public:
+	std::string GetData(const std::string& key) const noexcept
+	{
+		return "Very Big Data String: " + key;
+	}
+};
+
+class CacheDB : public VeryHeavyDatabase
+{
+	std::map<std::string, std::string> _cache_;
+	std::unique_ptr<VeryHeavyDatabase> _real_DB_;
+public:
+	CacheDB(std::unique_ptr<VeryHeavyDatabase> real_DB) : _real_DB_(std::move(real_DB)) {}
+
+	std::string GetData(const std::string& key)
+	{														
+		if (_cache_.find(key) == _cache_.end())				
+		{
+			std::cout << "Read from real DB - ";				
+			_cache_[key] = _real_DB_->GetData(key);			
+		}
+		else
+		{
+			std::cout << "Read from cache - ";
+		}
+		return _cache_.at(key);								
+	}														
+};
+
+
+class OneShotDB : public VeryHeavyDatabase 
+{
+	std::unique_ptr<VeryHeavyDatabase> _real_object_;
+	size_t _shots_;
+public:
+	explicit OneShotDB(std::unique_ptr<VeryHeavyDatabase> real_object, size_t shots = 1) : _real_object_(std::move(real_object)), _shots_(shots){}
+	
+	std::string GetData(const std::string& key)
+	{
+		if (_shots_ != 0)
+		{
+			std::cout << "Read from real DB - ";				
+			_shots_--;
+			return _real_object_->GetData(key);
+		}
+		else
+		{
+			return "Error";
+		}
+	}
+};
+
+
+std::unique_ptr<CacheDB> init_cache()
+{
+	auto original_db = std::make_unique<VeryHeavyDatabase>(); 
+	return std::make_unique<CacheDB>(std::move(original_db));
+}
+
+int main()
+{
+	auto DB = init_cache();
+	std::cout << DB->GetData("key_1") << std::endl;
+	std::cout << DB->GetData("key_1") << std::endl;
+	std::cout << DB->GetData("key_2") << std::endl;
+
+	std::cout << std::endl;
+	auto limit_db = std::make_unique<OneShotDB>(std::move(DB), 2);
+	std::cout << limit_db->GetData("key") << std::endl;
+	std::cout << limit_db->GetData("key") << std::endl;
+	std::cout << limit_db->GetData("key") << std::endl;
+	std::cout << limit_db->GetData("key") << std::endl;
+
+	return 0;
+}
